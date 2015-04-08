cff50c8933032258fcc6cc5f1cd8d9224127c00d
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-04-09 05:07:31 +0700

    Form login

---
+++ autoTrans/LoginForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace autoTrans
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginGroupBox = new System.Windows.Forms.GroupBox();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.judulTiketLabel = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.loginGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginGroupBox
+            // 
+            this.loginGroupBox.Controls.Add(this.passwordTextBox);
+            this.loginGroupBox.Controls.Add(this.passLabel);
+            this.loginGroupBox.Controls.Add(this.userLabel);
+            this.loginGroupBox.Controls.Add(this.userTextBox);
+            this.loginGroupBox.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginGroupBox.Location = new System.Drawing.Point(23, 47);
+            this.loginGroupBox.Name = "loginGroupBox";
+            this.loginGroupBox.Size = new System.Drawing.Size(200, 89);
+            this.loginGroupBox.TabIndex = 0;
+            this.loginGroupBox.TabStop = false;
+            this.loginGroupBox.Text = "Login";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(94, 19);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(100, 23);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // judulTiketLabel
+            // 
+            this.judulTiketLabel.AutoSize = true;
+            this.judulTiketLabel.Font = new System.Drawing.Font("Motorwerk", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.judulTiketLabel.Location = new System.Drawing.Point(53, 18);
+            this.judulTiketLabel.Name = "judulTiketLabel";
+            this.judulTiketLabel.Size = new System.Drawing.Size(153, 26);
+            this.judulTiketLabel.TabIndex = 1;
+            this.judulTiketLabel.Text = "Welcome";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(7, 25);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(34, 16);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "User";
+            // 
+            // passLabel
+            // 
+            this.passLabel.AutoSize = true;
+            this.passLabel.Location = new System.Drawing.Point(10, 55);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(64, 16);
+            this.passLabel.TabIndex = 3;
+            this.passLabel.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(94, 50);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 23);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Masuk";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 181);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.judulTiketLabel);
+            this.Controls.Add(this.loginGroupBox);
+            this.Name = "LoginForm";
+            this.Text = "Aya Travel";
+            this.loginGroupBox.ResumeLayout(false);
+            this.loginGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox loginGroupBox;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label judulTiketLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
