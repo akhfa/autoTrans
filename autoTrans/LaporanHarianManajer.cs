5fc5f0561916f83a447009c786c329b601614e79
Fahmi Dumadi <fah.midum.adi@gmail.com>
2015-04-09 15:26:05 +0700

    laporan harian

---
+++ autoTrans/LaporanHarianManajer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autoTrans
+{
+    public partial class LaporanHarianManajer : Form
+    {
+        public LaporanHarianManajer()
+        {
+            InitializeComponent();
+            LokasicomboBox.Items.Add("Bandung");
+            LokasicomboBox.Items.Add("Depok");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LaporanHarianManajer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
